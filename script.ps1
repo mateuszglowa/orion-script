a0abed548541fd17bac3436cd9f23502381cded0
Mateusz Glowa <glowa.mateusz@gmail.com>
2023-11-16 21:46:21 +0100

    adding tightvnc

--- script.ps1
+++ script.ps1
@@ -6,5 +6,10 @@ Invoke-WebRequest -URI "https://github.com/duncanthrax/scream/releases/download/
 Invoke-WebRequest -URI "https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe" -OutFile "C:\Temp\SteamSetup.exe";
 Invoke-WebRequest -URI "https://www.tightvnc.com/download/2.8.81/tightvnc-2.8.81-gpl-setup-64bit.msi" -OutFile "C:\Temp\tightvnc-2.8.81-gpl-setup-64bit.msi";
 
-C:\Temp\SteamSetup.exe /S
+Set-Location "C:\Temp\"
+
+SteamSetup.exe /S
+
+msiexec /i tightvnc-2.7.1-setup-64bit.msi /quiet /norestart
+
 Stop-Transcript
