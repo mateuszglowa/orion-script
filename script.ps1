e21d016b37c5f5cba046a6262a30864684b73605
Mateusz Glowa <glowa.mateusz@gmail.com>
2023-11-16 22:23:08 +0100

    steamSetup

--- script.ps1
+++ script.ps1
@@ -5,7 +5,7 @@ Param(
 
 # Folders
 Start-Transcript -Path "C:\Temp\transcript0.txt" -NoClobber
-New-Item -ItemType Directory c:\Temp
+#New-Item -ItemType Directory c:\Temp
 
 Invoke-WebRequest -URI "https://github.com/duncanthrax/scream/releases/download/4.0/Scream4.0.zip" -OutFile "C:\Temp\Scream4.0.zip";
 Invoke-WebRequest -URI "https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe" -OutFile "C:\Temp\SteamSetup.exe";
@@ -13,8 +13,8 @@ Invoke-WebRequest -URI "https://www.tightvnc.com/download/2.8.81/tightvnc-2.8.81
 
 Set-Location "C:\Temp\"
 
-msiexec /i SteamSetup.exe /S
+.\SteamSetup.exe /S
 
-msiexec /i tightvnc-2.7.1-setup-64bit.msi /quiet /norestart SET_USEVNCAUTHENTICATION=1 VALUE_OF_USEVNCAUTHENTICATION=1 SET_PASSWORD=1 VALUE_OF_PASSWORD=$pass SET_USECONTROLAUTHENTICATION=1
+msiexec /i .\tightvnc-2.7.1-setup-64bit.msi /quiet /norestart SET_USEVNCAUTHENTICATION=1 VALUE_OF_USEVNCAUTHENTICATION=1 SET_PASSWORD=1 VALUE_OF_PASSWORD=$pass SET_USECONTROLAUTHENTICATION=1
 
 Stop-Transcript
