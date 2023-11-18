12cfbfb216fd6d069d5a029b192bd21f03b99935
Mateusz Glowa <glowa.mateusz@gmail.com>
2023-11-18 11:13:37 +0100

    adding custom resolution utility

--- script.ps1
+++ script.ps1
@@ -10,6 +10,7 @@ Start-Transcript -Path "C:\Temp\transcript0.txt" -NoClobber
 Invoke-WebRequest -URI "https://github.com/duncanthrax/scream/releases/download/4.0/Scream4.0.zip" -OutFile "C:\Temp\Scream4.0.zip";
 Invoke-WebRequest -URI "https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe" -OutFile "C:\Temp\SteamSetup.exe";
 Invoke-WebRequest -URI "https://www.tightvnc.com/download/2.8.81/tightvnc-2.8.81-gpl-setup-64bit.msi" -OutFile "C:\Temp\tightvnc-2.8.81-gpl-setup-64bit.msi";
+Invoke-WebRequest -URI "https://www.lo4d.com/get-file/custom-resolution-utility/3cb7a4d0c5db8b72dfed4e43eda52da1/" -OutFile "C:\Temp\cru-1.5.2.zip";
 
 Set-Location "C:\Temp\"
 Write-Output "Installing Steam Client"
